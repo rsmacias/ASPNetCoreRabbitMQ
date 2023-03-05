5b890f5874e8b4f1f832a253d6c3b98da0261ead
Robert Macias <Robert.Macias@trialcard.com>
2023-03-04 19:23:36 -0500

    feat: Adding data access logic.

--- src/Producer/Program.cs
+++ src/Producer/Program.cs
@@ -1,6 +1,12 @@
+using Producer.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<OrderDbContext>(options => options.UseInMemoryDatabase("ASPNetCoreRabbitMQ_DB"));
+
 // Add services to the container.
+builder.Services.AddScoped<IOrderDbContext, OrderDbContext>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
