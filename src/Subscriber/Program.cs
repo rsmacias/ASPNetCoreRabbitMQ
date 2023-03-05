969b7d077f4034a9682a50ca0a3ae3d9ea818596
Robert Macias <Robert.Macias@trialcard.com>
2023-03-04 20:14:04 -0500

    feat: set up the subscriber (console).

--- src/Subscriber/Program.cs
+++ src/Subscriber/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+var text = string.Empty;
+
+while(text != "quit") {
+    Console.WriteLine("Enter 'quit' for exiting.");
+    text = Console.ReadLine();
+}
